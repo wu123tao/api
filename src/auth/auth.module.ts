8b2337edc2eec9cbaa5a8d300393f5ba45b59102
wutao <q.1041154165@qq.com>
2023-06-17 16:59:52 +0800

    fix: auth模块调用users模块的service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,10 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypegooseModule } from 'nestjs-typegoose';
 import { User } from 'src/users/db/user.model';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  imports: [TypegooseModule.forFeature([User])],
+  imports: [TypegooseModule.forFeature([User]), UsersModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
