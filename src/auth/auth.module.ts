0419abc9743c6cfb49535c54be5ec315d43496ec
wutao <q.1041154165@qq.com>
2023-06-17 17:52:00 +0800

    feat: 配置jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,24 @@ import { AuthController } from './auth.controller';
 import { TypegooseModule } from 'nestjs-typegoose';
 import { User } from 'src/users/db/user.model';
 import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { jwtConstants } from './constant';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  imports: [TypegooseModule.forFeature([User]), UsersModule],
+  imports: [
+    TypegooseModule.forFeature([User]),
+    UsersModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      // expiresIn: 60秒
+      signOptions: { expiresIn: 60 },
+    }),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
+  exports: [AuthService],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,12 +3,37 @@ import { ReturnModelType } from '@typegoose/typegoose';
 import { InjectModel } from 'nestjs-typegoose';
 import { User } from 'src/users/db/user.model';
 import { LoginDto } from './schema/login';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectModel(User) private readonly userModel: ReturnModelType<typeof User>,
+    private readonly jwtService: JwtService,
   ) {}
+
+  // JWT验证 - Step 2: 校验用户信息
+  async validateUser(loginDto: LoginDto) {
+    console.log('JWT验证 - Step 2: 校验用户信息');
+    const user = await this.userModel.findOne(loginDto);
+
+    if (!user) throw new HttpException('账号不存在', HttpStatus.NOT_ACCEPTABLE);
+
+    return user;
+  }
+
+  // JWT验证 - Step 3: 处理 jwt 签证
+  async certificate(loginDto: LoginDto) {
+    const payload = { ...loginDto };
+    console.log('JWT验证 - Step 3: 处理 jwt 签证');
+
+    try {
+      const token = this.jwtService.sign(payload);
+      return token;
+    } catch (error) {
+      throw new HttpException('生成token失败', HttpStatus.BAD_REQUEST);
+    }
+  }
 
   async login(loginDto: LoginDto) {
     const userInfo = await this.userModel.findOne({
